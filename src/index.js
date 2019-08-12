3131d0775f033a632f19f90296e940cd70542f97
glen oliff <glen.oliff@gmail.com>
2019-08-12 12:08:44 -0600

    Adjusted to switch turns

--- src/index.js
+++ src/index.js
@@ -2,32 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
+// class Square extends React.Component {
+//     render() {
+//       return (
+//         <button className="square" 
+//                 onClick={() => this.props.onClick()} >
+
+//           {this.props.value}
+
+//         </button>
+//       );
+//     }
+//   }
+
+  function Square(props) {
       return (
-        <button className="square" 
-                onClick={() => this.props.onClick()} >
-
-          {this.props.value}
-
+        <button className="square" onClick={() => props.onClick()}>
+          {props.value}
         </button>
       );
-    }
   }
-  
+
   class Board extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         }
     }
 
     handleClick(i) {
         const newSquares = this.state.squares.slice();
-        newSquares[i] = 'X';
-        this.setState({squares: newSquares});
+        newSquares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: newSquares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -40,7 +52,7 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
