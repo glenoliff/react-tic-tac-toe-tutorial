61cd398961b994736d0a31bd864ff0c88c0dcd98
glen oliff <glen.oliff@gmail.com>
2019-08-13 15:39:14 -0600

    Implemented history

--- src/index.js
+++ src/index.js
@@ -53,6 +53,7 @@ import './index.css';
             history: [
                 { squares: Array(9).fill(null), }
             ],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
@@ -90,7 +91,7 @@ import './index.css';
 
     handleClick(i) {
 
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const newSquares = current.squares.slice();
 
@@ -106,17 +107,35 @@ import './index.css';
             history: history.concat([{
                 squares: newSquares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
         });
     }
 
     render() {
 
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = this.calculateWinner(current.squares);
-    
+
+        const moves = history.map((step, move) => {
+            const desc  = move ? 'Go to move #' + move : 'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status;
+
         if (winner) {
           status = 'Winner: ' + winner;
         } else {
@@ -134,22 +153,10 @@ import './index.css';
             </div>
             <div className="game-info">
               <div>{status}</div>
-              <ol>{/* TODO */}</ol>
+              <ol>{moves}</ol>
             </div>
           </div>
         );
-
-    //   return (
-    //     <div className="game">
-    //       <div className="game-board">
-    //         <Board />
-    //       </div>
-    //       <div className="game-info">
-    //         <div>{/* status */}</div>
-    //         <ol>{/* TODO */}</ol>
-    //       </div>
-    //     </div>
-    //   );
     }
   }
   
